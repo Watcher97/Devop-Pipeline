e0c4688d90af3a8994152f3b268378de8201646e
Dennis <dennis4380@gmail.com>
2025-11-01 10:46:42 +0800

    Feature: Add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello Devops!');
+console.log('This is the Devops Pipeline index file.');
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Starting Devops tests...');
+setTimeout(() => {console.log('Waiting 3 seconds');}, 3000);
+console.log('Devops tests completed.');
